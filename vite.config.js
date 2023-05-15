2abb9c9a3ee6972a6ad06bbdf3c79a360e878d2b
Kate2111 <noskova2111@mail.ru>
2023-05-15 17:58:57 +0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Vue_food',
-  plugins: [vue()],
-})
+export default defineConfig(
+  {
+    plugins: [
+      vue(),
+    ],
+    base: '/Vue_food',
+  
+  })
